315961480001962fa750efbbd8be3cee7236fcb8
YutingFENG <yuting.feng@centralesupelec.fr>
2020-03-16 16:06:23 +0100

    Add fct getTipiStudentMongoAuth

--- twinews/utils.py
+++ twinews/utils.py
@@ -1,2 +1,14 @@
-def yuting():
-    print("yuting")
+from systemtools.basics import *
+from systemtools.printer import *
+from systemtools.file import *
+from systemtools.location import *
+from systemtools.system import *
+from datatools.dataencryptor import *
+from datastructuretools.hashmap import *
+
+def getTipiStudentMongoAuth(*args, **kwargs):
+    password = getDataEncryptorSingleton()["mongoauth"]['titanv']
+    host = '127.0.0.1'
+    return ('student', password['student'], host)
+
+(user, password, host) = getTipiStudentMongoAuth()
