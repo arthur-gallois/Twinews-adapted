79a56872bebb8d76b8271828164aa8932ce23d37
hayj <hay.julien1@gmail.com>
2020-03-24 17:43:10 +0100

    adding distance metrics

--- twinews/utils.py
+++ twinews/utils.py
@@ -14,6 +14,9 @@ from nlptools.news import parser as newsParser
 import copy
 import pickle
 import gzip
+from math import log2
+from math import sqrt
+from numpy import asarray
 
 def getMongoHost():
 	weAreAtLRI = False
@@ -225,6 +228,27 @@ def getNewsFilteredSentences(*args, **kwargs):
 	return getNewsField(*args, field='filtered_sentences', **kwargs)
 def getNewsFilteredText(*args, **kwargs):
 	return getNewsField(*args, field='filtered_text', **kwargs)
+
+def getDistances(xvectors, yvectors, metric='cosin', logger=None, verbose=False):
+    """
+        metric can be 'cosine', 'euclidean', 'kl', 'js'
+    """
+    # Kullback–Leibler and Jensen-Shannon divergence: 
+    def kl_divergence(p, q):
+        return sum(p[i] * log2(p[i]/q[i]) for i in range(len(p)))
+    def js_divergence(p, q):
+        m = 0.5 * (p + q)
+        return 0.5 * kl_divergence(p, m) + 0.5 * kl_divergence(q, m)
+    if metric == 'cosine':
+        pass
+    elif metric == 'euclidean':
+        pass
+    elif metric == 'kl':
+        metric = kl_divergence
+    elif metric == 'js':
+        metric = js_divergence
+    distances = pairwise_distances(xvectors, yvectors, metric=metric)
+    return distances
 
 if __name__ == '__main__':
 	evalData = getEvalData(1, maxExtraNews=0, maxUsers=100)
