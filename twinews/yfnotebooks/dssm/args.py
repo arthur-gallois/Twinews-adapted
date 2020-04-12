7555e029f23cde90e914a2a47c613ef05baa8245
YutingFENG <yuting.feng@centralesupelec.fr>
2020-04-12 20:36:56 +0200

    DSSM

---
+++ twinews/yfnotebooks/dssm/args.py
@@ -0,0 +1,16 @@
+#-*-coding:utf-8-*-
+seq_length = 7427
+
+char_embedding_size = 100
+
+learning_rate = 0.0005
+
+keep_prob = 0.7
+
+vocab_size = 7427
+
+class_size = 2
+
+epochs = 50
+
+batch_size = 100  # TODO need to decide a new batch size avoid collapsing
